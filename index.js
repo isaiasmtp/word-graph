3e686eda20002508926e6324de4af27507050adc
isaiasmtp <isaias.pontes@ccc.ufcg.edu.br>
2022-09-30 01:19:24 -0300

    adding public folder

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const app = express()
-app.use(express.static(path.join(__dirname + "/client/build")))
+app.use(express.static(path.join(__dirname + "/public")))
 
 
 app.listen(PORT)
