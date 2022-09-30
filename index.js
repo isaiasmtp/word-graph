f0b1b3228b03963a1bb7be9de1e5213fd1b5242f
isaiasmtp <isaias.pontes@ccc.ufcg.edu.br>
2022-09-30 01:34:44 -0300

    Trying backend

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const path = require('path')
+const cors =  require('cors')
 
-const PORT = process.env.PORT || 3000
+
+const PORT = process.env.PORT || 3001
 
 const app = express()
-app.use(express.static(path.join(__dirname + "/public")))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());   
+app.use(cors())
+
+app.use(express.static(path.join(__dirname + "/client/build")))
+
+
+app.get('/world', async (req, res) => {
+    res.send({teste: '1'})
+})
+
+
 
 
 app.listen(PORT)
